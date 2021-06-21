216f8a9eb9439cae1e2317ff5bed7b43fc01456f
caterinaburanelli <caterina.buranelli@studenti.unipd.it>
2021-06-21 16:19:01 +0200

    Update obstacle_model_100.py

--- tests/obstacle_model_100.py
+++ tests/obstacle_model_100.py
@@ -233,7 +233,7 @@ def test_walkway_benchmark(n, half_len, half_width, mode, run=-1, visual=True):
   # run 50 simulations
    # save the data
     # make plots
-"""
+
 layouts = ["pillars"]
 peop_num = np.linspace(20,120,20)
 sim_num = 50
@@ -262,4 +262,3 @@ for layout in layouts :
     print('{} DONE'.format(layout))
     dfs_layout.append(layout_df)
 final_df = pd.concat(dfs_layout)
-"""
